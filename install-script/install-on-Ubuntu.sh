12ea37955ea72a939234cb32e30942d501d69dba
jamac <jamelcn@gmail.com>
2014-02-15 08:04:03 +0800

    add install script

---
+++ install-script/install-on-Ubuntu.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# This script will install my personal software on ubuntu and also 
+# perform some common configurataions. 
+
+# install cairo-dock
+# install script for firefox: pentadactyl, foxyproxy, adblock, netvideohunter, set gfwlist for foxyproxy
+# install goagent
+# install chromium
+# for cocos2d-x: install cocos2d-x, lua, tolua++, android ndk, android sdk, ant
+# install flash
+# install powerline fonts, fantasque-sans-mono
+# install vim7.4(with graphic, lua, python and so on turn on), pyclewn, 
+# install vim snippets
+# setup all dotfiles
+# install unix-tools: ag, 
+# for java, instal jdk,
+# install compiz
+# install git, svn, rabbitvcs
+# install fcitx-sougoupinyin
+# config terminal
